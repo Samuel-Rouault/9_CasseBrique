b80713b339fb1ae097901dbfe10a81f48cc77c1e
ROUAULT Samuel <samuel.rouault@artfx34.lan>
2023-05-26 19:16:01 +0200

    Base du code

--- 9_CasseBrique/game/src/Main.cpp
+++ 9_CasseBrique/game/src/Main.cpp
@@ -1,7 +1,31 @@
 #include "raylib.h"
 
+void load();
+void unload();
+void update();
+void draw();
 
 int main()
 {
     return 0;
+}
+
+void update() 
+{
+
+}
+
+void draw() 
+{
+
+}
+
+void load() 
+{
+
+}
+
+void unload() 
+{
+
 }
